57307216e2c91115f0df1f46c87d9d4fd5dbf79a
jasonhuang23 <jasonhuang2343@gmail.com>
2020-10-27 13:25:34 -1000

    Removed inline css styling
    Removed inline css styling and imported Navigation.css

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Navigation.css';
 
 const Navigation = () => {
     return (
-        <nav style={{height:'1em', padding:'1em', backgroundColor:'#282c34'}}>
-          <Link style={{padding:'1em', color:'white'}} to="/">Dashboard</Link>
-          <Link style={{padding:'1em', color:'white'}} to="/data">Raw Data</Link>
+        <nav>
+          <Link id = "navbarLink"  to="/">Dashboard</Link>
+          <Link id = "navbarLink"  to="/data">Raw Data</Link>
         </nav>
     );
 };
