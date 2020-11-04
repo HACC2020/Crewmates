503badbc231017e7653c5c71318e916f3bb67925
jasonhuang23 <jasonhuang2343@gmail.com>
2020-11-03 23:22:04 -1000

    Added first row in ProjectDasboard
    Added first row to display business value vs. project risk graph

--- src/components/Dashboard/ProjectsDashboard.js
+++ src/components/Dashboard/ProjectsDashboard.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { useData } from '../../providers/DataProvider';
+import { Container, Row, Col} from 'react-bootstrap';
 
 const ProjectsDashboard = () => {
-    const { 
+    const {
         projects,
         calculateProjectStatusMetric,
         calculateBusinessValueMetric,
         calculateProjectRiskMetric
     } = useData();
+
+    const colors = {
+        powderblue: {backgroundColor:'powderblue'},
+        skyblue: {backgroundColor: 'skyblue'},
+        steelblue: {backgroundColor: 'steelblue'},
+        white: {backgroundColor: 'white'}
+    };
 
     const [projectsMetrics, setProjectsMetrics] = useState({
         projectStatusMetric: calculateProjectStatusMetric(projects),
@@ -28,10 +36,28 @@ const ProjectsDashboard = () => {
 
     return (
     <>
-        <div>
-            <h1>TODO: Projects Dashboard</h1>
-        </div>
-    </>   
+
+        <Container fluid>
+        <Row>
+        <Col xs={12} md={7} style={{...colors.white, padding:'4em', paddingBottom:'2em'}}>
+        <h1 style={{marginBottom:'1em'}}>Business Value vs. Project Risk</h1>
+        </Col>
+        <Col xs={12} md={5} style={{...colors.skyblue, padding: '3em', paddingBottom:'2em'}}>
+        <h1 >Business Criticality Metric</h1>
+        <Row>
+        <Col md={6} style={{backgroundColor:'orange', paddingBottom:'2em'}}>
+        <p style={{}}>What is Business Value</p>
+        </Col>
+        <Col md={6} style={{backgroundColor:'green', paddingBottom:'2em'}}>
+        <h3>What is Project Risk?</h3>
+        </Col>
+        </Row>
+        </Col>
+        </Row>
+
+
+        </Container>
+    </>
     );
 };
 
