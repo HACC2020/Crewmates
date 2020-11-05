e961b9d24baafc7a12c679805192f82f4c19e41b
jasonhuang23 <jasonhuang2343@gmail.com>
2020-11-04 15:09:09 -1000

    Added card component
    Added card component using react bootstrap

--- src/components/Dashboard/ProjectsDashboard.js
+++ src/components/Dashboard/ProjectsDashboard.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useData } from '../../providers/DataProvider';
-import { Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col, Card} from 'react-bootstrap';
 
 const ProjectsDashboard = () => {
     const {
@@ -42,8 +42,8 @@ const ProjectsDashboard = () => {
         <Col xs={12} md={7} style={{...colors.white, padding:'4em', paddingBottom:'2em'}}>
         <h1 style={{marginBottom:'1em'}}>Business Value vs. Project Risk</h1>
         </Col>
-        <Col xs={12} md={5} style={{backgroundColor: 'var(--theme-color-3)', padding: '3em', paddingBottom:'2em'}}>
-        <h1 >Business Criticality Metric</h1>
+        <Col xs={12} md={5} style={{backgroundColor: 'var(--theme-color-4)', padding: '3em', paddingBottom:'2em'}}>
+        <h1 style={{color:'white'}}>Business Criticality Metric</h1>
         <Row>
         <Col md={6} style={{paddingBottom:'2em'}}>
         <h4 style={{color: 'white'}}>What is Business Value?</h4>
@@ -56,14 +56,26 @@ const ProjectsDashboard = () => {
         </Row>
 
         <Row>
-        <Col xs={12} md={12} style={{backgroundColor: 'var(--theme-color-1)', padding:'4em', paddingBottom:'2em'}}>
+        <Col xs={12} md={12} style={{backgroundColor: 'var(--theme-color-3)', padding:'4em', paddingBottom:'2em'}}>
         <h1 style={{marginBottom:'1em', color:'white'}}>Mini Project Metric Cards</h1>
+        <Card style={{ width: '18rem' }}>
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+            <Card.Link href="#">Card Link</Card.Link>
+            <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>
         </Col>
         </Row>
 
         <Row>
-        <Col xs={12} md={6} style={{...colors.steelblue, padding:'4em', paddingBottom:'2em'}}>
-        <h1 style={{marginBottom:'1em'}}>Canceled vs Completed projects</h1>
+        <Col xs={12} md={6} style={{backgroundColor: 'var(--theme-color-1)', padding:'4em', paddingBottom:'2em'}}>
+        <h1 style={{marginBottom:'1em', color:'white'}}>Canceled vs Completed projects</h1>
         </Col>
 
         <Col xs={12} md={6} style={{padding:'4em', paddingBottom:'2em'}}>
