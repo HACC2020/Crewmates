662f6eab4863cf99ec27d12f83e6cad7d0023dc9
Jan <jeligio808@gmail.com>
2020-11-05 02:50:42 -1000

    Fix styling

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -7,41 +7,15 @@ import './Dashboard.css';
 
 const Dashboard = () => {
     // To display the chosen dashboard
-    const [currentDashboard, setCurrentDashboard] = useState(0);
-
-    const dashboards = [ <ApplicationsDashboard/>, <ProjectsDashboard/>, <MISDashboard/>];
 
     return (
         <>
         <header className="dashboard-header">
-        State of Hawai'i Executive Branch ETS IT Portfolio
+            Hawai'i State Executive Branch Application Portfolio
         </header>
-            {/* <Container>
-                <Row>
-                    <Col md={1}></Col>
-                    <Col sm={4} md={3}>
-                        <Button variant="link" style={{ padding: '1em' }}
-                            onClick={()=>setCurrentDashboard(0)}>
-                            IT Applications
-                        </Button>
-                    </Col>
-                    <Col sm={4} md={3}>
-                        <Button variant="link" style={{ padding: '1em' }}
-                            onClick={()=>setCurrentDashboard(1)}>
-                            IT Projects
-                        </Button>
-                    </Col>
-                    <Col sm={4} md={4}>
-                        <Button variant="link" style={{ padding: '1em' }}
-                            onClick={()=>setCurrentDashboard(2)}>
-                            Major Information Systems
-                        </Button>
-                    </Col>
-                    <Col md={1}></Col>
-                </Row>
-            </Container> */}
-
-            {dashboards[currentDashboard]}
+        <ApplicationsDashboard/>
+        <ProjectsDashboard/>
+        <MISDashboard/>
         </>
     );
 };
