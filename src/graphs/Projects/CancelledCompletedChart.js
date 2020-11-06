4b0b675a455db1f4c3294c2020094a2281ef06ba
Joshua Hartmann <45891115+joshuabh@users.noreply.github.com>
2020-11-05 17:28:17 -1000

    added legend

--- src/graphs/Projects/CancelledCompletedChart.js
+++ src/graphs/Projects/CancelledCompletedChart.js
@@ -5,7 +5,8 @@ import { pie, arc } from 'd3';
 const CancelledCompletedChart = () => {
     const { projects, calculateProjectCancelledCompleted } = useData();
     const { cancelled, projectedCompleted, missing } = calculateProjectCancelledCompleted(projects);
-    const data = [ {"key": "a", "value": projectedCompleted}, {"key": "b", "value": cancelled}, {"key": "c", "value": missing} ]
+    const data = [ {"key": "Projected Completion", "value": projectedCompleted}, {"key": "Cancelled", "value": cancelled},
+                    {"key": "Missing Data", "value": missing} ]
 
     const width = 150;
     const height = 150;
@@ -24,7 +25,7 @@ const CancelledCompletedChart = () => {
             .innerRadius(0)
             .outerRadius(radius)
 
-        const transform = `translate(${width/2}, ${height/2})`;
+        const transform = `translate(${(width / 3)}, ${height / 2})`;
 
         return (
             <g  transform={transform} >
@@ -54,9 +55,25 @@ const CancelledCompletedChart = () => {
 
     }
 
+    const legend = () => {
+        const x = width / 3 * 2;
+        const y = height / 4;
+        return (
+            <>
+            {colors.map((d, index) => 
+                <g fill={colors[index]}>
+                    <circle cx={x} cy={y + index * 6} r={2} />
+                    <text x={x + 5} y={y + 2 + index * 6} fontSize={'5px'}>{data[index].key}</text>
+                </g>
+            )}
+            </>
+        );
+    }
+
     return (
     <svg style={{overflow: 'visible'}} fontSize={`2`} viewBox={`0, 0, ${width}, ${height}`}>
         {pieChart()}
+        {legend()}
     </svg>);
 }
 
