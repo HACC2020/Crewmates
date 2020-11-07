9781c9df0e7d3d36b92ddd0aad729011a57d7beb
Jan <jeligio808@gmail.com>
2020-11-07 01:09:57 -1000

    Refactor IT Roadmap Timeline

--- src/graphs/ITRoadmapTimeline/ITRoadmapTimeline.js
+++ src/graphs/ITRoadmapTimeline/ITRoadmapTimeline.js
@@ -5,10 +5,9 @@ import _ from 'lodash';
 import moment from 'moment';
 import Popper from '@material-ui/core/Popper';
 import { useSpring, animated } from 'react-spring/web.cjs'; // web.cjs is required for IE 11 support
-import Paper from '@material-ui/core/Paper';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
+import { getTheYear, getMonthsFromDate, buildRoadmapData } from './functions';
+import ApplicationCard from './ApplicationCard';
+
 import './ITRoadmapTimeline.css';
 
 const ITRoadmapTimeline = () => {
@@ -35,41 +34,29 @@ const ITRoadmapTimeline = () => {
 
     const data = buildRoadmapData(filteredApplications, minYear);
 
-    // Function to spit out the correct y coordinate of bar
-    // const y = scaleBand()
-    //     .domain(range(data.length))
-    //     .range([0,height - margin.bottom - margin.top])
-    //     .padding(0.2);
+    // Function to spit out the correct y coordinate on graph
     const y = scaleBand()
         .domain(data.map(d => d.id))
         .range([0, height - margin.bottom - margin.top])
         .padding(.15);
 
+    // Function to spit out the correct x coordinate on graph
     const x = scaleLinear()
         .domain([0, totalYearsInMonths])
         .range([0, width - margin.left - margin.right]);
 
+    // Function to spit out correct x coordinate of the bar ticks
     const xMonthScale = scaleLinear()
         .domain([minYear, maxYear])
         .range([0, width - margin.left - margin.right]);
 
-    const Bar = ({data}) => {
-        const [darken, setDarken] = useState(false);
-        return (        
-            <g  onMouseEnter={()=>setDarken(true)}
-                onMouseLeave={()=>setDarken(false)} 
-                transform={`translate(${x(data.start)},${y(data.id)} )`}>
-                <rect 
-                    height={y.bandwidth()}
-                    width={x(data.end) - x(data.start)}
-                    fill={!darken ? data.color : data.highlightColor}/>
-                <text transform={`translate(5, ${y.bandwidth()/2})`} fontSize={'.5em'} fill={data.textColor}>{data.label}</text>
-            </g>);
-    };
 
+    /** This is the component that renders the bars on the graph
+     * and the popover when you hover over a bar.
+     */
     const BarPopover = ({data}) => {
         const [darken, setDarken] = useState(false);
-        const [anchorEl, setAnchorEl] = React.useState(null);
+        const [anchorEl, setAnchorEl] = useState(null);
         const open = Boolean(anchorEl);
 
         const handleMouseEnter = (event) => {
@@ -126,8 +113,10 @@ const ITRoadmapTimeline = () => {
             .tickSize(4);
 
         const bottomAxis = axisBottom(xMonthScale)
-            .ticks(15)
-            .tickFormat(value => `${value}`);
+            .ticks(years.length)
+            .tickValues(years)
+            .tickFormat(value => `${value}`)
+            .tickSize(4);
 
         const topAxisRef = axis => {
             axis && topAxis(select(axis));
@@ -148,8 +137,8 @@ const ITRoadmapTimeline = () => {
         );
     }
 
+    // Draws a line at the current date
     const CurrentDateMarker = () => {
-
         const today = getMonthsFromDate(minYear, moment().format("YYYY-MM-DD"));
         return (
         <g transform={`translate(${margin.left}, ${margin.top})`}>
@@ -180,134 +169,9 @@ const ITRoadmapTimeline = () => {
         </div>);
 };
 
-const getTheYear = date => {
-    const dateMoment = moment(date);
-    return dateMoment.get('year');    
-}
-
-// date format: YYYY-MM-DD
-const getMonthsFromDate = (minYear, date) => {
-    const dateAsMoment = moment(date);
-    const year = dateAsMoment.get('year');
-    const month = dateAsMoment.get('month');
-
-    return ((year - minYear) * 12) + month;
-}
-
-// Function to generate a tick for the current date
-const getCurrentMonths = minYear => {
-    const today = moment().format("YYYY-MM-DD");
-    console.log(today);
-    return getMonthsFromDate(minYear, today);
-}
-
-const buildRoadmapData = (applications, minYear) => {
-    const data = [];
-    _.forEach(applications, app => {
-        let color = 'var(--missing-data-color)';
-        let textColor = 'black';
-        let highlightColor = 'var(--missing-data-color-active)';
-
-        const rating = fieldToRating(app.timeTag);
-        if(rating === 1) {
-            color = 'var(--warning-color-red)';
-            textColor = 'white';
-            highlightColor = 'var(--warning-color-red-active)'
-        } 
-        if(rating === 2) {
-            color = 'var(--warning-color-orange)';
-            highlightColor = 'var(--warning-color-orange-active)'
-        } 
-        if(rating === 3) {
-            color = 'var(--warning-color-yellow)';
-            highlightColor = 'var(--warning-color-yellow-active)'
-        } 
-        if(rating === 4) {
-            textColor = 'white';
-            color = 'var(--warning-color-green)';
-            highlightColor = 'var(--warning-color-green-active)'
-        } 
-
-        const appData = {
-            id: app.id,
-            start: getMonthsFromDate(minYear, app['lifecycle:active']),
-            end: getMonthsFromDate(minYear, app['lifecycle:endOfLife']),
-            label: app.name,
-            department: app.ownerAgencyName,
-            color: color,
-            textColor: textColor,
-            highlightColor: highlightColor,
-            data: app
-        };
-
-        data.push(appData); // Insert into new array
-    });
-    return data;
-};
-
-/* Given a field and an application return an integer representing 
-    a rating of that field.
-    
-    Ex: 
-    const field = 'timeTag';
-    const fieldValue = 'Invest';
-
-    const rating = fieldToRating(field, fieldValue);
-    // 4
-*/
-const fieldToRating = (fieldValue) => {
-    let rating = 0;
-    switch (fieldValue) {
-
-        // If it's a businessCriticality value
-        case 'missionCritical':
-            rating = 1;
-            break;
-        case 'businessCritical':
-            rating = 2;
-            break;
-        case 'businessOperational':
-            rating = 3;
-            break;
-        case 'administrativeService':
-            rating = 4;
-            break;
-
-        // If it's TIME tag
-        case 'Eliminate':
-            rating = 1;
-            break;
-        case 'Migrate':
-            rating = 2;
-            break;
-        case 'Tolerate':
-            rating = 3;
-            break;
-        case 'Invest':
-            rating = 4;
-            break;
-
-        // If it's functionalFit
-        case 'poor':
-            rating = 1;
-            break;
-        case 'insufficient':
-            rating = 2;
-            break;
-        case 'adequate':
-            rating = 3;
-            break;
-        case 'excellent':
-            rating = 4;
-            break;
-        default:
-            rating = 0;
-            break;
-    }
-
-    return rating;
-};
-
+/**
+ * Function to wrap a component in a transition.
+ */
 const Fade = React.forwardRef(function Fade(props, ref) {
     const { in: open, children, onEnter, onExited, ...other } = props;
     const style = useSpring({
@@ -330,32 +194,6 @@ const Fade = React.forwardRef(function Fade(props, ref) {
         {children}
       </animated.div>
     );
-  });
-
-const ApplicationCard = ({data}) => {
-    return (
-        <Card>
-        <CardContent>
-          <Typography color="textSecondary" gutterBottom>
-            {moment(data['lifecycle:active']).format('YYYY MMMM DD')} 
-            {`     -     `}
-            {moment(data['lifecycle:endOfLife']).format('YYYY MMMM DD')}
-          </Typography>
-          <Typography variant="h5" component="h2">
-            {data.name}
-          </Typography>
-          <Typography color="textSecondary">
-            {data.ownerAgencyName}
-          </Typography>
-          <Typography variant="body2" component="p">
-            JASON FILL OUT THE REST OF THIS
-            <br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-      </Card>
-    );
-}
-
+});
 
 export default ITRoadmapTimeline;
