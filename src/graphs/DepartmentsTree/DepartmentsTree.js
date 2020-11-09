2a6a27e83c617d1f7d548c04229a96ded7ea418d
Jan <jeligio808@gmail.com>
2020-11-09 13:14:09 -1000

    Fix incorrect Departments Tree data

--- src/graphs/DepartmentsTree/DepartmentsTree.js
+++ src/graphs/DepartmentsTree/DepartmentsTree.js
@@ -18,10 +18,9 @@ const DepartmentsTree = () => {
         height: '100vh'
     }
     
-    console.log(root);
     return (
     <div style={{...dimensionStyle, border:'1px solid black'}}>
-        <Tree translate={{x:30, y:30}}
+        <Tree translate={{x:300, y:300}}
             scaleExtent={{min:0.1, max: 5}}
             zoom={1} orientation="horizontal" pathFunc="diagonal" transitionDuration={250} initialDepth={1} data={root}/>
     </div>
@@ -57,7 +56,7 @@ const buildTree =  (departments, applications) => {
 
     let topLevelDepartments = _.filter(departments, d => !d.parent);
     let subDepartments = _.filter(departments, d => d.parent);
-
+    console.log(subDepartments);
     root.children.push({
         name:'Legislature'
     });
@@ -73,14 +72,18 @@ const buildTree =  (departments, applications) => {
     });
 
     _.forEach(subDepartments, d => {
+        // Find the index of the sub department in root.children
         const i = _.findIndex(root.children, (child) => {
-            return child.name === d.name;
+            return child.name === d.parent;
         });
 
+        // Find the sub department's applications
         const childApps = _.filter(applications, app => {
             return d.name === app.ownerAgencyName
         });
-        if(i > 0) {
+
+        // Insert into the children array of its parent
+        if(i >= 0) {
             root.children[i].children.push({
                 name: d.name,
                 children: childApps
