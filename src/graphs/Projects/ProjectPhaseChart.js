4678bb8a839072b00495b2a0062c9dcdb4a8ab14
Joshua Hartmann <45891115+joshuabh@users.noreply.github.com>
2020-11-19 21:07:49 -1000

    Finished project phase chart

--- src/graphs/Projects/ProjectPhaseChart.js
+++ src/graphs/Projects/ProjectPhaseChart.js
@@ -11,9 +11,9 @@ const ProjectPhaseChart = () => {
 
     const width = 200;
     const height = 135;
-    const margin = 40;
-    const radius = width / 3 - 25;  // pie chart to take up about 1/3 of the width
-    const colors = ['blue', 'red', 'green', 'yellow', 'pink', 'purple', 'grey'];
+    const margin = 25;
+    const radius = width / 3 - margin;  // pie chart to take up about 1/3 of the width
+    const colors = ['var(--theme-color-2)', 'var(--theme-color-2)', 'var(--theme-color-1)', 'green', 'red', 'var(--theme-color-1)', 'grey'];
 
     const pieChart = () => {
         let myPie = pie()
@@ -21,20 +21,28 @@ const ProjectPhaseChart = () => {
 
         let dataReady = myPie(data);
 
+        // the arc for the pie chart
         let myArc = arc()
             .innerRadius(23)
             .outerRadius(radius)
 
+        // the outer edge of the circle, helper to draw the line correctly
+        let textArc = arc()
+            .innerRadius(radius)
+            .outerRadius(radius)
+
+        // move the center of the pie chart to the center of the svg container
         const transform = `translate(${(width / 2)}, ${height / 2})`;
         
+        // move the text to the correct spot with the line just touching it
         const textTransform = i => {
-            let arc = myArc.centroid(dataReady[i]);
+            let arc = textArc.centroid(dataReady[i]);
             let x = arc[0];
             let y = arc[1];
             let h = Math.sqrt((x * x) + (y * y));
 
-            arc[0] += x/h * 14;
-            arc[1] += y/h * 20 + i;
+            arc[0] > -2 ? arc[0] = 35 : arc[0] = -56; // x coordinate
+            arc[1] += y/h * 20 + 2*i; // y coordinate
 
             return arc;
         }
@@ -53,14 +61,7 @@ const ProjectPhaseChart = () => {
         return (
             <g  transform={transform} >
                 {colors.map((c, index) => 
-                    <React.Fragment key={`${c}`}>
-                        <line 
-                            x1={myArc.centroid(dataReady[index])[0]}
-                            x2={textTransform(index)[0]}
-                            y1={myArc.centroid(dataReady[index])[1]}
-                            y2={textTransform(index)[1]}
-                            stroke='black'
-                            strokeWidth='.5px' />
+                    <React.Fragment key={`${c}${index}`}>
                         <path
                             className='line'
                             d={myArc(dataReady[index])}
@@ -76,6 +77,13 @@ const ProjectPhaseChart = () => {
                             textAnchor={anchor(index)}>
                             {`${data[index].key} ${data[index].value}`}
                         </text>
+                        <line 
+                            x1={textArc.centroid(dataReady[index])[0]}
+                            x2={textTransform(index)[0]}
+                            y1={textArc.centroid(dataReady[index])[1]}
+                            y2={textTransform(index)[1]-2}
+                            stroke='lightgrey'
+                            strokeWidth='.5px' />
                     </React.Fragment>
                     )
                 }
@@ -135,7 +143,7 @@ const buildData = (projects) => {
     });
 
     return [ {key: 'Planning', value: planning}, {key: 'Approved', value: approved}, {key: 'Projected to have Started', value: projectedStart},
-                {key: 'Projected to have been Completed', value: projectedCompletion}, {key: 'Cancelled', value: cancelled},
+                {key: 'Projected to be Completed', value: projectedCompletion}, {key: 'Cancelled', value: cancelled},
                 {key: 'Future Project', value: future}, {key: 'Missing Data', value: missing} ];
 }
 
